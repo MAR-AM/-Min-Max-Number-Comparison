5370c2b3dfda75ebd87c958befa7d564faa45a2d
MARIEM-AMZIL <147107786+MAR-AM@users.noreply.github.com>
2024-04-27 15:11:10 +0100

    Min-Max-Number-Comparison.js

---
+++ Min-Max-Number-Comparison.js
@@ -0,0 +1,16 @@
+var test1=(x,y)=>
+        {
+            return Math.min(x,y)
+        }
+
+var test2=(x,y)=>
+        {
+            return Math.max(x,y)
+        }
+
+
+        var a = prompt("entre a : ")
+        var b = prompt("entre b : ")
+
+        console.log(test1(a,b))
+        console.log(test2(a,b))
